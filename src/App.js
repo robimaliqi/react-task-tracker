4132de9a2e8bc20f2c7ec531ee86cc427f88254f
Robi <robi.maliqi@hotmail.co.uk>
2022-07-17 21:11:50 +0100

    added the css to the index.css file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello From React</h1>
+    <div className="container">
+
+      <Header />
     </div>
   );
 }
+
 
 export default App;
