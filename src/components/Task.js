98bad2d9c356d4ee47acf3badd5e822e26151309
Robi <robi.maliqi@hotmail.co.uk>
2022-07-19 22:13:15 +0100

    added delete function

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,12 @@
 import { FaTimes } from 'react-icons/fa'
 
-const Task = ({ task }) => {
+const Task = ({ task, onDelete }) => {
   return (
     <div claddName='task'>
       <h3>
-        {task.text}<FaTimes style={{ color: 'red', cursor: 'pointer' }}/>
+        {task.text}<FaTimes style={{ color: 'red', cursor: 
+        'pointer' }} 
+        onClick={() => onDelete(task.id)}/>
       </h3>
       <p>{task.day}</p>
       
