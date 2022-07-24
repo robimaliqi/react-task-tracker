724509e1c77c98a6b035f03f80edc947e51f0c66
Robi <robi.maliqi@hotmail.co.uk>
2022-07-24 22:51:05 +0100

    added toggle remider function which changes the reminder from true to flase if you double click on the taks and back to false if you double click on it again, although this doesn't show in the UI just in the console so far, will soon show in the UI with the next implentation.

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = ({ title }) => {
     console.log('Click')
   }
   return (
-    <header claddName='header'>
+    <header className='header'>
       <h1>{title}</h1>
       <Button color='green' text='Add' onClick={onClick} />
     </header>
