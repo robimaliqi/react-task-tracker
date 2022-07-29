b74511a0dc095beb7f6a9aabf9ba5c1370fccafb
Robi <robi.maliqi@hotmail.co.uk>
2022-07-29 21:42:22 +0100

    created AddTask.js file which adds a a new task to the task tracker, it is now stateful and can see the outcome in the console but need to add the functionality in the app.js file

--- src/App.js
+++ src/App.js
@@ -1,56 +1,55 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Tasks from './components/Tasks'
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
     {
       id: 1,
-      text: 'Dcotors Appointment',
-      day: 'Feb 5th at 2:30pm',
+      text: "Dcotors Appointment",
+      day: "Feb 5th at 2:30pm",
       reminder: true,
     },
     {
       id: 2,
-      text: 'Meeting at School',
-      day: 'Feb 6th at 1:30pm',
+      text: "Meeting at School",
+      day: "Feb 6th at 1:30pm",
       reminder: true,
     },
     {
       id: 3,
-      text: 'Food Shopping',
-      day: 'Feb 5th at 2:30pm',
+      text: "Food Shopping",
+      day: "Feb 5th at 2:30pm",
       reminder: false,
-    }
-  ])
+    },
+  ]);
 
   // Delete Task
   const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id))
-  }
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
   // Toggle Reminder
   const toggleReminder = (id) => {
     setTasks(
-      tasks.map((task) => 
-        task.id === id ? {...task, reminder:
-        !task.reminder } : task
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
       )
-    )
-  }
+    );
+  };
 
   return (
     <div className="container">
       <Header />
+      <AddTask />
       {tasks.length > 0 ? (
-        <Tasks  tasks={tasks} onDelete=
-        {deleteTask}  onToggle={toggleReminder} /> 
-        ) : (
-          'No tasks to show'
-        )}
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No tasks to show"
+      )}
     </div>
   );
 }
-
 
 export default App;
