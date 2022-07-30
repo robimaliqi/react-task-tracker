18221bac7781bcc93ff8fe8120881047cce51e4e
Robi <robi.maliqi@hotmail.co.uk>
2022-07-30 18:21:23 +0100

    adjusted the Add Task function so the user input for a new task gets added to the server and then added to the UI

--- src/App.js
+++ src/App.js
@@ -25,10 +25,22 @@ function App() {
   };
 
   //Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 1000) + 1;
-    const newTask = { id, ...task };
-    setTasks([...tasks, newTask]);
+  const addTask = async (task) => {
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-type": "applicaion/json",
+      },
+      body: JSON.stringify(task),
+    });
+
+    const data = await res.json();
+
+    setTasks([...tasks, data]);
+
+    // const id = Math.floor(Math.random() * 1000) + 1;
+    // const newTask = { id, ...task };
+    // setTasks([...tasks, newTask]);
   };
 
   // Delete Task
